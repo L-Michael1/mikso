a9fa316eaf3c890c6c1a86b94106710ca4821221
Michael Lam <m.lam08@outlook.com>
2023-11-05 12:24:02 -0500

    feat: framer motion up component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
     <div>
-      <p>Navbar: The current theme is: {theme}</p>
-      <div>
-        <button onClick={() => setTheme("light")}>Light Mode</button>
-      </div>
-      <div>
-        <button onClick={() => setTheme("dark")}>Dark Mode</button>
-      </div>
+      {mounted && (
+        <>
+          <p>Navbar: The current theme is: {theme}</p>
+          <div>
+            <button onClick={() => setTheme("light")}>Light Mode</button>
+          </div>
+          <div>
+            <button onClick={() => setTheme("dark")}>Dark Mode</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import Layout from "~/components/Layout";
+import Up from "~/motions/Up";
 
 const Home = () => {
   return (
     <Layout description="Home">
-      mikso
-      <Link href="/about">About</Link>
+      <Up delay={0.2}>mikso</Up>
+      <Up delay={0.4}>
+        <Link href="/about">About</Link>
+      </Up>
     </Layout>
   );
 };
