037f44d665650a8cee4deebad3a007dbec04ae20
Michael Lam <m.lam08@outlook.com>
2023-11-05 11:43:28 -0500

    feat: layout (#2)
    * feat: layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+interface iLayoutProps {
+  children: React.ReactNode;
+  description: string;
+  icon?: string;
+}
+
+const Layout = ({ children, description, icon }: iLayoutProps) => {
+  const currentRoute = useRouter().pathname;
+
+  return (
+    <>
+      <Head>
+        <title>
+          {currentRoute === "/" ? "mikso" : `mikso // ${currentRoute.slice(1)}`}
+        </title>
+        <meta name="description" content={description} />
+        <meta name="author" content="Michael Lam" />
+        <meta name="keywords" content="mikso, nextjs, t3, portfolio" />
+        {icon && <link rel="icon" href={icon} />}
+      </Head>
+      <main>
+        <Navbar />
+        <div className="mx-auto max-w-7xl">{children}</div>
+        <Footer />
+      </main>
+    </>
+  );
+};
+
+export default Layout;
